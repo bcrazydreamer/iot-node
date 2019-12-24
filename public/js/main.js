4cc325795c579e220d682731b83e4a08276f2e40
bcrazydreamer <bcrazydreamer@gmail.com>
2019-12-24 19:25:34 +0530

    ^

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,5 @@
 var apiPoint = "http://"+window.location.host+"/api";
+var pin = 7;
 function actionClick(ob){
     var iotLoader = $("#iot-main-loader");
     iotLoader.addClass("center-all");
@@ -6,7 +7,7 @@ function actionClick(ob){
     $.ajax({
         url: apiPoint + "/act",
         type: "POST",
-        data: {state : ob,pin : 7},
+        data: {state : ob,pin : pin},
         success: function(data, textStatus, jqXHR) {
             var state = (ob === true || ob === "true") ? "ON" : "OFF";
             $("#iot-state-msg").text("State is "+state);
@@ -27,6 +28,35 @@ function actionClick(ob){
         }
     });
 }
+function checkState(){
+    $.ajax({
+        url: apiPoint + "/read",
+        type: "POST",
+        data: {pin : pin},
+        success: function(data, textStatus, jqXHR) {
+            console.log(data);
+            $("#iot-switch-ctnr").removeClass("dis-none")
+            // var state = (ob === true || ob === "true") ? "ON" : "OFF";
+            // $("#iot-state-msg").text("State is "+state);
+            // iotLoader.removeClass("center-all");
+            // iotLoader.css({"display" : "none"});
+            // if(state === "ON"){
+            //     $("#iot-on-switch").addClass("dis-none");;
+            //     $("#iot-off-switch").removeClass("dis-none");
+            // } else {
+            //     $("#iot-on-switch").removeClass("dis-none");
+            //     $("#iot-off-switch").addClass("dis-none");
+            // }
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            $("#iot-state-msg").text("Something went wrong try after some time");
+            iotLoader.removeClass("center-all");
+            iotLoader.css({"display" : "none"});
+            $("#iot-switch-ctnr").removeClass("dis-none")
+        }
+    });
+}
+checkState();
 
 $( document ).ready(function() {
     var iotLoader = $("#iot-main-loader");
